22fe1bd1846de7b3f46f7908d0aa2cc7622accff
Nasar Hussain <husainasar1412@gmail.com>
2026-02-27 16:57:47 +0530

    This is updated Git

---
+++ src/test/java/Git_Package_cicd/ForGitPractice.java
@@ -0,0 +1,14 @@
+package Git_Package_cicd;
+import org.testng.annotations.Test;
+
+public class ForGitPractice {
+	
+	
+	@Test	
+	public void printName()
+	{
+		
+		System.out.println("Hello Nasar Husain --- Welcome in Git world");
+	}
+
+}
